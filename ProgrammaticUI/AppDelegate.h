072b4da9a726826a5f9bee6c9c01543da51a4021
Navjeet Dhaliwal <nsd@ualberta.ca>
2018-09-18 18:33:46 -0600

    Initial Commit

---
+++ ProgrammaticUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgrammaticUI
+//
+//  Created by Navjeet Dhaliwal on 2018-09-18.
+//  Copyright © 2018 Navjeet Dhaliwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProgrammaticUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgrammaticUI
+//
+//  Created by Navjeet Dhaliwal on 2018-09-18.
+//  Copyright © 2018 Navjeet Dhaliwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
